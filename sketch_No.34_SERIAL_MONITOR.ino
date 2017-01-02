8f3e3d2ab0fe24a504896f4fba0c897fff7f47f2
MetalEinstein <rasmus123102@gmail.com>
2017-01-02 09:32:38 +0100

    Add files via upload

---
+++ sketch_No.34_SERIAL_MONITOR.ino
@@ -0,0 +1,22 @@
+int pinPot = A0;
+
+void setup(){
+  pinMode(pinPot, INPUT);
+  Serial.begin(9600);
+}
+
+
+int potValue;
+byte data[2];
+
+void loop() {
+  potValue = analogRead(pinPot);
+  // delay(100);
+ Serial.write(potValue/ 256);
+   delay(30);
+ Serial.write(potValue % 256);
+   delay(30);
+  //Serial.print("value = ");
+  //Serial.print(potValue);
+  
+}
